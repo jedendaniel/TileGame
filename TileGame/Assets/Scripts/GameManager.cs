66bc626ad8a5330cf00b70b174d760f135957988
Dd <jedendaniel@gmail.com>
2018-08-03 19:33:46 +0200

    Add camera movement, add clear console utility tool

--- TileGame/Assets/Scripts/GameManager.cs
+++ TileGame/Assets/Scripts/GameManager.cs
@@ -2,19 +2,40 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 [Serializable]
 public class GameManager : MonoBehaviour {
 
-    
+    public Camera mainCamera;
     public Map map;
+
+    Vector3 rightClickPosition;
     
     void Start () {
         map.GenerateMap();
 	}
 	void Update () {
-        if (Input.GetMouseButtonUp(0)) Select();
+        if (Input.GetMouseButtonDown(0))
+        {
+            rightClickPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(rightClickPosition == Input.mousePosition)
+                Select();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            MoveCameraHorizontally();
+        }
+        if(Input.GetAxis("Mouse Scroll") != 0)
+        {
+            MoveCameraVertically();
+        }
     }
 
     void Select()
@@ -27,5 +48,19 @@ public class GameManager : MonoBehaviour {
             string[] name = ourHitObject.name.Split('-');
             map.SelectTile(int.Parse(name[1]), int.Parse(name[2]));
         }
+    }
+
+    void MoveCameraHorizontally()
+    {
+        mainCamera.transform.position -= new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+    }
+
+    void MoveCameraVertically()
+    {
+        mainCamera.transform.position -= Vector3.up * Input.GetAxis("Mouse Scroll");
+        if (mainCamera.transform.position.y > 10)
+            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, 10, mainCamera.transform.position.z);
+        if (mainCamera.transform.position.y < 5)
+            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, 5, mainCamera.transform.position.z);
     }
 }
---
+++ TileGame/Assets/Scripts/MyUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEditor;
+
+public static class MyUtility
+{
+    public static void ClearConsole()
+    {
+        var assembly = Assembly.GetAssembly(typeof(SceneView));
+        var type = assembly.GetType("UnityEditor.LogEntries");
+        var method = type.GetMethod("Clear");
+        method.Invoke(new object(), null);
+    }
+}
